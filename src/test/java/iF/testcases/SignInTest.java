6cb97662b310e0c87d0e4914ce7ab2e0fdaf0a72
unknown <bijayrana@gmail.com>
2019-02-22 11:13:38 +0530

    5th commit

--- src/test/java/iF/testcases/SignInTest.java
+++ src/test/java/iF/testcases/SignInTest.java
@@ -16,7 +16,7 @@ import iF.utilities.Utilities;
 public class SignInTest {
 
  
-	//Test
+	
 	@Test(dataProviderClass=Utilities.class,dataProvider="dp")
 	public void signInTest(Hashtable<String,String> data)  {
 		
