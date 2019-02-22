bef468dc3915548ba0f478145faf9bac9050a894
unknown <bijayrana@gmail.com>
2019-02-22 09:08:23 +0530

    second commit

--- src/main/java/iF/base/Constants.java
+++ src/main/java/iF/base/Constants.java
@@ -3,7 +3,7 @@ package iF.base;
 public class Constants {
 	
 	//Configuration
-		public static  String browser = "firefox";
+		public static  String browser = "chrome";
 		public static final String testsiteurl = "https://investorforce-qa.msci.com/";
 		public static long implicitwait=10;
 		
